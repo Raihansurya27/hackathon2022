f96ac8ac32445294b336a8f6a061da95e5404c7c
Anggiat19 <anggiat167@gmail.com>
2022-10-22 12:42:07 +0700

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -213,6 +213,32 @@
     <form action="/login-cpanel" class="" method="post">
     <div class="log-in-form-v3-left__inputs">
     <div class="flex flex-column mb-15">
+        <form action="{{url('login')}}" method="POST" role="form" class=" form-khusus" data-aos="fade-up" enctype="multipart/form-data" data-aos-delay="100">
+            @csrf
+            <div class="row">
+                <div class="col-lg-12 col-md-6 form-group">
+                    <input type="email" name="email" class="form-control @error('email')is-invalid
+                    @enderror"  placeholder="Email" data-rule="email" data-msg="Please enter a valid email" value="{{old('email')}}">
+                    @error('email')
+                        {{$message}}
+                    @enderror
+                </div>
+                <div class="col-lg-12 col-md-6 form-group mt-3 mt-md-0">
+                    <input type="password" class="form-control @error('password')is-invalid
+                    @enderror" name="password"  placeholder="Password" data-rule="password" data-msg="Please enter a valid password">
+                    @error('password')
+                        {{$message}}
+                    @enderror
+                </div>
+            </div>
+            <div class="text-center mt-2">
+                <button type="submit" class=" mt-1">Masuk</button>
+            </div>
+            <div class="text-center mt-5">
+                <p class=" mb-0">Sudah punya akun?</p>
+                <a href="{{url('/register')}}">Daftar</a>
+            </div>
+        </form>
 
 
 
