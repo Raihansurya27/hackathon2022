7f407123f731995acc0166fa92ab17493b96c24e
syahlan1i <msyahlan336@example.com>
2022-10-22 18:45:22 +0700

    s

--- resources/views/dashboard/layout/header.blade.php
+++ resources/views/dashboard/layout/header.blade.php
@@ -3,17 +3,17 @@
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="navbar-nav">
+    <div class="navbar-nav" >
       <div class="nav-item text-nowrap">
         @empty(auth()->user()->picture)
-        <img src="{{asset('img/noprofile.png')}}" alt="{{auth()->user()->name}}" style="width: 30px; height: 30px;" class="">
+        <img src="{{asset('img/dfprofile.png')}}" alt="{{auth()->user()->name}}" style="width: 30px; height: 30px;" class="">
         @else
         <img src="{{asset('img/profil/'.auth()->user()->picture)}}" alt="{{auth()->user()->name}}" style="width: 30px; height: 30px;" class="img-fluid">
         @endempty
         <form action="/logout" method="POST">
-            @csrf
-            <button class="btn btn-primary">Logout</button>
-        </form>
+          @csrf
+          <button class="btn btn-primary" style="margin-right: 20px">Logout</button>
+      </form>
       </div>
     </div>
   </header>
